75eabf12a55c3fd9652e031f2b7cab6589521a75
Emanuele Zignale <zignale92@gmail.com>
2022-07-10 16:14:38 +0200

    Update index.js

--- keeper-app/src/index.js
+++ keeper-app/src/index.js
@@ -12,6 +12,9 @@ ReactDOM.render(
     </div>,
     document.getElementById("root")
 );
+
+
+
 //3. Create a Header.jsx component that renders a <header> element
 //to show the Keeper App name in an <h1>.
 //4. Create a Footer.jsx component that renders a <footer> element
