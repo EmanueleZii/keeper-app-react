42aa7acc0eb156517c31041f4a33bd53740fa435
Emanuele Zignale <zignale92@gmail.com>
2022-07-10 16:11:29 +0200

    keeper base

---
+++ keeper-app/src/component/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Footer(){
+    return <footer>
+        <span>copyright 2022</span>
+    </footer>
+}
+
+
+export default Footer;
---
+++ keeper-app/src/component/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+function Note(){
+    return <div>
+        <h1>Keeper App</h1>
+
+        <p>lorem islpum</p>
+
+    </div>
+}
+
+export default Note;
